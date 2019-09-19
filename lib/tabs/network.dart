aa0eb5cb810b11eb2977122e8c54f89d32d6871b
huhp <huhp@tingyun.com>
2019-09-19 15:42:45 +0800

    添加相关的网络部分的布局到相关的内容之中

--- lib/tabs/network.dart
+++ lib/tabs/network.dart
@@ -10,17 +10,52 @@ class Network extends StatefulWidget{
 }
 
 class NetworkState extends State<Network>{
+
+  Widget createSelectItem(String libName){
+    return new Row(
+      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      children: <Widget>[
+        Expanded(
+          flex: 2,
+          child : new Text(libName),
+        ),
+        Expanded(
+            flex: 1,
+            child : new Icon(Icons.check, color: Colors.grey,)
+        ),
+      ],
+    );
+  }
+
+  Widget createTag(String tagName){
+    return new Container(
+      child: new Row(
+        children: <Widget>[
+          new Text(tagName, style: TextStyle(fontSize: 26)),
+        ],
+      ),
+      color: Colors.grey[500],
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return new Scaffold(
       body: new Container(
 //        child: SafeArea(
-          child: new Row(
+          child: new Column(
 //          text从左还是右开始显示
             textDirection: TextDirection.ltr,
             children: <Widget>[
-              new Text('请选择类库')
+              createTag('请选择网络库'),
+              createSelectItem('dio 库'),
+              createSelectItem('http 库'),
+              createTag('请选择协议'),
+              createSelectItem('HTTP'),
+              createSelectItem('HTTPS'),
+              createTag('Response'),
             ],
+
           ),
         )
 //      ),
